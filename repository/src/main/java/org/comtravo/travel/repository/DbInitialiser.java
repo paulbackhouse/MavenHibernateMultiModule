00bdaf00b4fc9b468d405982374cf094849e219b
Paul Backhouse <paul.backhouse@comtravo.com>
2022-02-16 14:17:28 +0100

    Added BookingEntity and data preloading
    - new entity in domain layer
    - new ImportBookings method in dbinitialiser
    - new interface to enforce created modified naming in entities

--- repository/src/main/java/org/comtravo/travel/repository/DbInitialiser.java
+++ repository/src/main/java/org/comtravo/travel/repository/DbInitialiser.java
@@ -1,10 +1,12 @@
 package org.comtravo.travel.repository;
 
 import java.math.BigInteger;
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Random;
 
+import org.comtravo.travel.domain.entities.BookingEntity;
 import org.comtravo.travel.domain.entities.UserEntity;
 import org.comtravo.travel.repository.implementation.BaseRepository;
 import org.hibernate.Session;
@@ -14,6 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DbInitialiser extends BaseRepository {
+
+    // java doesn't do lambda expression as method paramr
+    // this is how you define it - because the java compiler - basically is lazy
+    interface IFor {
+        void Each(Integer i);
+    }
     
     private static final Logger logger = LoggerFactory.getLogger(DbInitialiser.class);
 
@@ -36,7 +44,7 @@ public class DbInitialiser extends BaseRepository {
 
         }
         catch (Exception e) {
-            logger.error("Failed to initialize database", e);
+            logger.error("Failed to initialise database", e);
         }
         finally {
             session.close();
@@ -46,6 +54,11 @@ public class DbInitialiser extends BaseRepository {
 
     private static boolean HasImportedRows(Session session, String tableName) {
 
+        // hibernate uses its own query language, hql - which is basically dog shite
+        // why would you not just use pure sql? 
+        // the better question is WHY SHOULD YOU BE WRITING TEXT AT ALL WITH AN ORM!!!???
+        // oh yeh, because hibernate is crap as well, wait until you see the "criteria" nonsense to query with conditions!
+        // is hands down, the better ORM - System.Linq makes it easy to query - java/hibernate - its 2022!
         var sql = String.format("select count(*) FROM %s tbl", tableName);
         var query = session.createSQLQuery(sql);
         var count = ((BigInteger)query.uniqueResult()).longValue();
@@ -55,11 +68,22 @@ public class DbInitialiser extends BaseRepository {
         return count > 0;
     }
 
-
     private static void LoadData(Session session) {
 
         ImportUsers(session);
+        ImportBookings(session);
 
+    }
+
+    private static void For(Integer amount, IFor iterator) {
+        for (var i = 0; i < amount; i++) {
+            iterator.Each(i);
+        }
+    }
+
+    private static Timestamp GetSqlTimestamp() {
+        java.util.Date date = new java.util.Date();
+        return new Timestamp(date.getTime()); 
     }
 
     private static void ImportUsers(Session session) {
@@ -73,14 +97,12 @@ public class DbInitialiser extends BaseRepository {
 
             var givenNameLength = givenNames.length;
             var lastNameLength = lastNames.length;
+            var now = GetSqlTimestamp();
             var rdm = new Random();
-
-            java.util.Date date = new java.util.Date();
-            var now = new Timestamp(date.getTime()); 
 
             session.beginTransaction();
 
-            for (var i = 0; i < 50; i++) {
+            For(50, i -> {
                 var rdmGivenNameIdx = rdm.nextInt(givenNameLength + 0);
                 var rdmLastNameIdx = rdm.nextInt(lastNameLength + 0);
                 var givenName = givenNames[rdmGivenNameIdx];
@@ -97,15 +119,65 @@ public class DbInitialiser extends BaseRepository {
     
                     session.save(user);
                     createdUsernames.add(username);
-                }    
-           }
+                }                 
+            });
 
             session.getTransaction().commit();
-
         }
     }
 
     private static void ImportBookings(Session session) {
+
+        if (!HasImportedRows(session, "bookings")) {
+            logger.info("Importing rows in 'bookings' table");
+
+            var query = session.createSQLQuery("select tbl.id FROM users tbl");
+            var userIds = query.getResultList();
+
+            String[] departures = { "Paris", "Berlin", "LA", "Singapore", "Sidney", "Madrid", "London", "Rome"};
+            String[] destinations = { "Nice", "Hamburg", "New York", "Moscow", "Glasgow", "Barcelona", "Turin", "Lisbon"};
+            String[] years = {"2022", "2023", "2024" };
+            String[] months = {"03", "04", "05", "06", "07", "08", "09", "10", "11"};
+            String[] days = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13",  "16", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27" };
+
+            var rdm = new Random();
+            var now = GetSqlTimestamp();
+
+            session.beginTransaction();
+
+            For(userIds.size(), i -> {
+                
+                var userId = userIds.get(i);
+                var bookingAmount = rdm.nextInt(10 + 0) + 1;
+
+                For(bookingAmount, idx -> {
+                    var departureIdx = rdm.nextInt(departures.length + 0);
+                    var destinationIdx = rdm.nextInt(destinations.length + 0);
+                    var departure = departures[departureIdx];
+                    var destination = destinations[destinationIdx];
+                    var dayIdx = rdm.nextInt(days.length + 0);
+                    var monthIdx = rdm.nextInt(months.length + 0);
+                    var yearIdx = rdm.nextInt(years.length + 0);
+                    var departOn = String.format("%1$s-%2$s-%3$s", years[yearIdx], months[monthIdx], days[dayIdx]);
+                    var travellers = rdm.nextInt(6 + 1) + 1;
+                    var booking = new BookingEntity();
+
+                    booking.setUserId((int)userId);
+                    booking.setCreated(now);
+                    booking.setModified(now);
+                    booking.setDeparture(departure);
+                    booking.setDestination(destination);
+                    booking.setDepart(Date.valueOf(departOn));
+                    booking.setTravellers((short)travellers);
+
+                    session.save(booking);
+
+                });
+
+            });
+
+            session.getTransaction().commit();
+        }
 
     }
 
