405e818393678089d7da91ea0d03a466971b9488
Paul Backhouse <paul.backhouse@comtravo.com>
2022-02-16 16:29:43 +0100

    Removed unused imports

--- webapi/src/main/java/org/comtravo/travel/controller/UserController.java
+++ webapi/src/main/java/org/comtravo/travel/controller/UserController.java
@@ -1,17 +1,22 @@
 package org.comtravo.travel.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.comtravo.travel.domain.aggregates.UserBookingsAggregate;
 import org.comtravo.travel.domain.dto.UserDto;
 import org.comtravo.travel.domain.services.IBookingService;
 import org.comtravo.travel.domain.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
-
-import javax.validation.Valid;
-import javax.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
